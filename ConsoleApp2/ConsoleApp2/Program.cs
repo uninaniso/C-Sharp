c1ff492f61d2b9b78255a6d9ae77bbc1853217ad
uninaniso <arthur.zamkvoyi@gmail.com>
2024-01-19 17:44:40 +0200

    add

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i < n; i++)
+            {
+                if (n % i == 0)
+                {
+                    int j = n / i;
+                    Console.WriteLine($"{i} * {j} = {n}");
+                }
+            }
+        }
+    }
+}
