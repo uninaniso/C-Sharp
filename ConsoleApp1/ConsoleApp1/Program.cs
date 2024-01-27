a53d504346ea564059ad4a7865f33baef469f420
uninaniso <arthur.zamkvoyi@gmail.com>
2024-01-27 17:34:29 +0200

    add

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,43 +10,17 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.Write("input the date in the format dd.mm.yyyy: ");
-            if (DateTime.TryParse(Console.ReadLine(), out DateTime userInputDate))
-            {
-                string season = GetSeason(userInputDate.Month);
-                string dayOfWeek = userInputDate.DayOfWeek.ToString();
+            Console.Write("input ");
+            int i = Convert.ToInt32(Console.ReadLine());
 
-                Console.WriteLine($"{season} {dayOfWeek}");
-            }
+            Console.Write("1 - c, 2 - f:");
+            int j = Convert.ToInt32(Console.ReadLine());
+
+            if (j == 1)
+                Console.WriteLine("C"+5/9*(i - 32));
+
             else
-            {
-                Console.WriteLine("Invalid date format!");
-            }
-        }
-
-        static string GetSeason(int month)
-        {
-            switch (month)
-            {
-                case 12:
-                case 1:
-                case 2:
-                    return "Winter";
-                case 3:
-                case 4:
-                case 5:
-                    return "Spring";
-                case 6:
-                case 7:
-                case 8:
-                    return "Summer";
-                case 9:
-                case 10:
-                case 11:
-                    return "Autumn";
-                default:
-                    return "Invalid Month";
-            }
+                Console.WriteLine("F" + 5 / 9 * (i + 32));
         }
     }
 }
